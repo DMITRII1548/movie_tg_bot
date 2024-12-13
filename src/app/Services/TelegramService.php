c93322b9cfcd6a45b6815aab84fecac9e4bac365
DMITRII1548 <korsunov2006@list.ru>
2024-12-13 19:55:18 +0600

    Preparation

--- src/app/Services/TelegramService.php
+++ src/app/Services/TelegramService.php
@@ -2,13 +2,39 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class TelegramService
 {
-    /**
-     * Create a new class instance.
-     */
+    private string $url = '';
+
     public function __construct()
     {
-        //
+        $this->prepareBaseUrl();
+    }
+
+    private function prepareBaseUrl(): void
+    {
+        $token = config('services.telegram.token');
+        $this->url = "https://api.telegram.org/bot$token/";
+    }
+
+    public function sendMessage(
+        string $text,
+        int $chatId,
+        ?array $replyMarkup = null,
+        string $parseMode = 'html',
+    ): void {
+        $data = [
+            'text' => $text,
+            'parse_mode' => $parseMode,
+            'chat_id' => $chatId,
+        ];
+
+        if ($replyMarkup) {
+            $query['reply_markup'] = $replyMarkup;
+        }
+
+        $res = Http::post("{$this->url}/sendMessage", $data);
     }
 }
